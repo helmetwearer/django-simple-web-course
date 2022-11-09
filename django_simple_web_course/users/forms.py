4fa123057132078437a800281065dc6a997ccb35
Daniel Petrikin <danpabsolute@gmail.com>
2022-11-09 00:14:15 -0500

    styled registration form. Base template and some other registration updates needed

--- django_simple_web_course/users/forms.py
+++ django_simple_web_course/users/forms.py
@@ -8,16 +8,22 @@ class CustomUserRegistrationForm(RegistrationForm):
         retval = super(CustomUserRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].required = True
         self.fields['first_name'].widget.attrs.update({'autofocus': 'autofocus',
-            'required': 'required', 'tabindex':'1'})
-        self.fields['first_name'].widget.attrs
+            'required': 'required', 'tabindex':'1', 'class': 'form-control form-control-user',
+            'placeholder':'First Name'})
         self.fields['last_name'].required = True
         self.fields['last_name'].widget.attrs['tabindex'] = 2
+        self.fields['last_name'].widget.attrs['class'] = 'form-control form-control-user'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
         self.fields[User.USERNAME_FIELD].widget.attrs['tabindex'] = 3
         self.fields[User.USERNAME_FIELD].widget.attrs.pop("autofocus", None)
-        self.fields[User.get_email_field_name()].widget.attrs['tabindex'] = 3
-        self.fields[User.get_email_field_name()].widget.attrs.pop("autofocus", None)
+        self.fields[User.USERNAME_FIELD].widget.attrs['class'] = 'form-control form-control-user'
+        self.fields[User.USERNAME_FIELD].widget.attrs['placeholder'] = 'Email'
         self.fields['password1'].widget.attrs['tabindex'] = 4
+        self.fields['password1'].widget.attrs['class'] = 'form-control form-control-user'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
         self.fields['password2'].widget.attrs['tabindex'] = 5
+        self.fields['password2'].widget.attrs['class'] = 'form-control form-control-user'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
 
         return retval
 
@@ -27,7 +33,6 @@ class CustomUserRegistrationForm(RegistrationForm):
             'first_name',
             'last_name',
             User.USERNAME_FIELD,
-            User.get_email_field_name(),
             'password1',
             'password2',
         ]
